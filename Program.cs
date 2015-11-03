c94684fd78bd8f1dc3e05eee4bdeffbf8a6c78fd
mmenage <matthieu.menage@imerir.com>
2015-11-03 17:40:23 +0100

    Mise a jour de lecture des coordonnées de la souris

--- Program.cs
+++ Program.cs
@@ -32,12 +32,55 @@ namespace Projet_NovaLynx
             //robotController.Connect("192.168.1.1");
 
             ConsoleKeyInfo info;
+            String bouton = device.Keyboard.GetKeyName(1);
+            Console.WriteLine(bouton);
             
+            bouton = device.Keyboard.GetKeyName(2);
+            Console.WriteLine(bouton);
+
+            bouton = device.Keyboard.GetKeyName(3);
+            Console.WriteLine(bouton);
+
+            bouton = device.Keyboard.GetKeyName(4);
+            Console.WriteLine(bouton);
+
+            bouton = device.Keyboard.GetKeyName(5);
+            Console.WriteLine(bouton);
+
+            bouton = device.Keyboard.GetKeyName(6);
+            Console.WriteLine(bouton);
+
+            bouton = device.Keyboard.GetKeyName(7);
+            Console.WriteLine(bouton);
+
+            bouton = device.Keyboard.GetKeyName(8);
+            Console.WriteLine(bouton);
+
+            bouton = device.Keyboard.GetKeyName(9);
+            Console.WriteLine(bouton);
+
+            bouton = device.Keyboard.GetKeyName(10);
+            Console.WriteLine(bouton);
+
+            bouton = device.Keyboard.GetKeyName(11);
+            Console.WriteLine(bouton);
+
+            bouton = device.Keyboard.GetKeyName(12);
+            Console.WriteLine(bouton);
+
+            bouton = device.Keyboard.GetKeyName(13);
+            Console.WriteLine(bouton);
+
+            bouton = device.Keyboard.GetKeyName(14);
+            Console.WriteLine(bouton);
+
+            bouton = device.Keyboard.GetKeyName(15);
+            Console.WriteLine(bouton);
+
             while (true)
             {
                 var translation = device.Sensor.Translation;
                 var rotation = device.Sensor.Rotation;
-                int name = device.Keyboard.Keys;
 
                 /* Affichage des translations et des rotations */
                 //Console.WriteLine("Translation: " + translation.X + " : " + translation.Y + " : " + translation.Z);
@@ -46,30 +89,9 @@ namespace Projet_NovaLynx
                 valeurX = translation.X / 2353;
                 valeurY = translation.Y / 2702;
                 valeurZ = translation.Z / 2648;
+                
 
-                //Console.WriteLine("X: " + valeurX);
-                //Console.WriteLine("Y: " + valeurY);
-                //Console.WriteLine("Z: " + valeurZ);
-
-                info = Console.ReadKey();
-
-                if (info.Key == ConsoleKey.UpArrow)
-                {
-                    Console.WriteLine("You pressed UP !");
-                }
-                else if (info.Key == ConsoleKey.DownArrow)
-                {
-                    Console.WriteLine("You pressed DOWN !");
-                }
-                else if (info.Key == ConsoleKey.LeftArrow)
-                {
-                    Console.WriteLine("You pressed LEFT !");
-                }
-                else if (info.Key == ConsoleKey.RightArrow)
-                {
-                    Console.WriteLine("You pressed RIGHT !");
-                }
-                System.Threading.Thread.Sleep(100);
+                System.Threading.Thread.Sleep(200);
             }
 
         }
