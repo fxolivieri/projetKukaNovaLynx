3132197db9a22e481f24f6dee82486aaf3672048
mmenage <matthieu.menage@imerir.com>
2015-11-03 16:11:37 +0100

    Récupère les coordonnées de X, Y et Z de la souris.

--- Program.cs
+++ Program.cs
@@ -1,57 +1,77 @@
-﻿using NLX.Robot.Kuka.Controller;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TDx.TDxInput;
 
-namespace KukkaMove
+namespace Projet_NovaLynx
 {
     class Program
     {
+        static TDx.TDxInput.Device device;
+        static NLX.Robot.Kuka.Controller.RobotController robotController;
+
         static void Main(string[] args)
         {
-            RobotController Robot = new RobotController();
-            Robot.Connect("192.168.1.1");
-            Console.WriteLine("Robot connecté ... ");
-            Console.WriteLine("Robot position : x:" + Robot.GetCurrentPosition().X + "; y:" + Robot.GetCurrentPosition().Y + "; z: " + Robot.GetCurrentPosition().Z);
 
+            /* Declaration des variables */
+            double x_Min = 0;
+            double y_Min = 0;
+            double z_Min = 0;
+            double x_Max = 0;
+            double y_Max = 0;
+            double z_Max = 0;
+            double valeurX;
+            double valeurY;
+            double valeurZ;
 
-            List<CartesianPosition> Trajectoire = new List<CartesianPosition>();
-            for (int i = 1; i <= 100; i++)
+            Console.WriteLine("Start...");
+            device = new TDx.TDxInput.Device();
+            device.Connect();
+            robotController = new NLX.Robot.Kuka.Controller.RobotController();
+            //robotController.Connect("192.168.1.1");
+
+            ConsoleKeyInfo info;
+            
+            while (true)
             {
+                var translation = device.Sensor.Translation;
+                var rotation = device.Sensor.Rotation;
+                int name = device.Keyboard.Keys;
 
-                Trajectoire.Add(new CartesianPosition
+                /* Affichage des translations et des rotations */
+                //Console.WriteLine("Translation: " + translation.X + " : " + translation.Y + " : " + translation.Z);
+                //Console.WriteLine("Rotation: " + rotation.X + " : " + rotation.Y + " : " + rotation.Z + "\n");
+
+                valeurX = translation.X / 2353;
+                valeurY = translation.Y / 2702;
+                valeurZ = translation.Z / 2648;
+
+                //Console.WriteLine("X: " + valeurX);
+                //Console.WriteLine("Y: " + valeurY);
+                //Console.WriteLine("Z: " + valeurZ);
+
+                info = Console.ReadKey();
+
+                if (info.Key == ConsoleKey.UpArrow)
                 {
-                    X = i + Robot.GetCurrentPosition().X,
-                    Y = i + Robot.GetCurrentPosition().Y,
-                    Z = i + Robot.GetCurrentPosition().Z,
-                    A = 0 + Robot.GetCurrentPosition().A,
-                    B = 0 + Robot.GetCurrentPosition().B,
-                    C = 0 + Robot.GetCurrentPosition().C
-
-                });
+                    Console.WriteLine("You pressed UP !");
+                }
+                else if (info.Key == ConsoleKey.DownArrow)
+                {
+                    Console.WriteLine("You pressed DOWN !");
+                }
+                else if (info.Key == ConsoleKey.LeftArrow)
+                {
+                    Console.WriteLine("You pressed LEFT !");
+                }
+                else if (info.Key == ConsoleKey.RightArrow)
+                {
+                    Console.WriteLine("You pressed RIGHT !");
+                }
+                System.Threading.Thread.Sleep(100);
             }
-            //Robot.PlayTrajectory(Trajectoire);
 
-            Robot.StartRelativeMovement();
-            CartesianPosition Test = new CartesianPosition
-            {
-
-                X = -1,
-                Y = 0,
-                Z = 0,
-                A = 0,
-                B = 0,
-                C = 0
-            };
-           
-            Robot.SetRelativeMovement(Test);
-            System.Threading.Thread.Sleep(2000);
-            Robot.StopRelativeMovement();
-
-            Device Mouse = new Device();
         }
     }
 }
